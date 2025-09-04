b80e22bad5cd3fdbaae06f41d374f0dd87c8a5d5
shauru1118 <sargsyan_ae09@mail.ru>
2025-09-05 01:02:55 +0300

    new-api-for-dz

--- flask_app.py
+++ flask_app.py
@@ -29,7 +29,7 @@ def get_user():
 
 if __name__ == '__main__':
     db.Init()
-    db.add_item(Person("albert", PHIS_MATH))
-    db.add_item(Person("ivan", PHIS_MATH))
-    db.add_item(Person("danya", INFO_MATH))
+    # db.add_item(Person("albert", PHIS_MATH))
+    # db.add_item(Person("ivan", PHIS_MATH))
+    # db.add_item(Person("danya", INFO_MATH))
     app.run("0.0.0.0", port=5000, debug=False)
