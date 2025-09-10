e9f2436685228114d79eb436fc346e69083ccb82
shauru1118 <sargsyan_ae09@mail.ru>
2025-09-10 15:13:17 +0300

    new-api-for-dz

--- flask_app.py
+++ flask_app.py
@@ -3,6 +3,7 @@ import dbfunc as db
 import utils
 
 app = Flask(__name__)
+db.Init()
 
 @app.route('/')
 def index():
@@ -42,6 +43,5 @@ def get_info():
 
 
 if __name__ == '__main__':
-    db.Init()
     app.run("0.0.0.0", port=8000, debug=True)
 
