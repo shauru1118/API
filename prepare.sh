7b5f18a33a94738cc7cb8a38bb30b9fb50ef5531
shauru1118 <sargsyan_ae09@mail.ru>
2025-09-15 22:02:54 +0300

    new-api-for-dz

--- prepare.sh
+++ prepare.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+echo "\n ---- > ls jsons < ---- \n"
 ls jsons 
 echo "------------"
+
+echo "\n ---- > rm jsons/*09.2025* < ---- \n"
 rm jsons/*09.2025*
 echo "------------"
+
+echo "\n ---- > ls jsons < ---- \n"
 ls jsons 
+echo "------------"
 
+echo "\n ---- > git pull < ---- \n"
 git pull
-
 echo "------------"
 
+echo "\n ---- > ls jsons < ---- \n"
 ls jsons
-
 echo "------------"
+
