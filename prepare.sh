b6edfb10336b943f05742d42ee9b199c0db6617b
shauru1118 <sargsyan_ae09@mail.ru>
2025-09-19 22:19:06 +0300

    new-api-for-dz

--- prepare.sh
+++ prepare.sh
@@ -12,3 +12,15 @@ echo "    ---- > tree < ----"
 echo ""
 tree 
 echo ""
+
+echo ""
+echo "    ---- > git pull < ----"
+echo ""
+git pull
+echo ""
+
+echo ""
+echo "    ---- > tree < ----"
+echo ""
+tree 
+echo ""
